c9436bcef6990233b26765a5165f3312624ec56b
Zhixin Piao <1029945092@qq.com>
2018-05-07 19:16:33 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/7 下午6:29
+# @Author  : Zhixin Piao 
+# @Email   : piaozhx@shanghaitech.edu.cn
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
